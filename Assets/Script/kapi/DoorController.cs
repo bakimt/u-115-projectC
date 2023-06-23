dbd30990b43305c19c7ac8968f12b7ca117df1e2
bakimt <mustafa-tb@outlook.com>
2023-06-24 02:10:15 +0300

    kapi

---
+++ Assets/Script/kapi/DoorController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Animator door = null;
+    [SerializeField] private bool openDoor = false;
+    [SerializeField] private bool closeDoor = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag ("Player"))
+        [
+            if (openDoor)
+            {
+                door.Play("acilKapi", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+
+            else if (closeDoor)
+            {
+                door.Play("kapanKapi", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        ]
+    }
+}
