4f4e98a5c386dbd5f6d75fea7b1c2fe5e6bf35ba
bakimt <mustafa-tb@outlook.com>
2023-06-27 03:26:40 +0300

    pressPlate

--- Assets/Script/kapi/triggerColliderDeneme.cs
+++ Assets/Script/kapi/triggerColliderDeneme.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class triggerColliderDeneme : MonoBehaviour
 {
     public GameObject doorMesh;
-
+    public GameObject pressPlate;
     void Start()
     {
         doorMesh.GetComponent<Animator>().SetBool("closeDoor", false);
         doorMesh.GetComponent<Animator>().SetBool("openDoor", false);
+        pressPlate.GetComponent<Animator>().SetBool("pressedPlate", false);
+        pressPlate.GetComponent<Animator>().SetBool("upPlate", false);
     }
 
     void OnTriggerEnter(Collider other)
     {
+        pressPlate.GetComponent<Animator>().SetBool("upPlate", false);
         doorMesh.GetComponent<Animator>().SetBool("closeDoor", false);
-        if (other.CompareTag("Player")) doorMesh.GetComponent<Animator>().SetBool("openDoor", true);
+        if (other.CompareTag("Player"))
+        {
+            doorMesh.GetComponent<Animator>().SetBool("openDoor", true);
+            pressPlate.GetComponent<Animator>().SetBool("pressedPlate", true);
+        }  
     }
     void OnTriggerExit(Collider other)
     {
+        pressPlate.GetComponent<Animator>().SetBool("pressedPlate", false);
         doorMesh.GetComponent<Animator>().SetBool("openDoor", false);
-        if (other.CompareTag("Player")) doorMesh.GetComponent<Animator>().SetBool("closeDoor", true);
+        if (other.CompareTag("Player"))
+        {
+            doorMesh.GetComponent<Animator>().SetBool("closeDoor", true);
+            pressPlate.GetComponent<Animator>().SetBool("upPlate", true);
+        }
     }
 }
