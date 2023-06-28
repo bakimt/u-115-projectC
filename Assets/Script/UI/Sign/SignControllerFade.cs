f9184937fe4021402667fd549ccf0611bd1d77b3
bakimt <mustafa-tb@outlook.com>
2023-06-29 00:50:22 +0300

    changes

---
+++ Assets/Script/UI/Sign/SignControllerFade.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignControllerFade : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private bool fadeIn = false;
+    [SerializeField] private bool fadeOut = false;
+    private bool isPlayerInRange = false;
+
+    public void Show()
+    {
+        fadeIn = true;
+    }
+
+    public void Hide()
+    {
+        fadeOut = true;
+    }
+
+    void Update()
+    {
+        if (fadeIn)
+        {
+            if (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime;
+                if (canvasGroup.alpha >= 1)
+                {
+                    fadeIn = false;
+                }
+            }
+        }
+        if (fadeOut)
+        {
+            if (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime;
+                if (canvasGroup.alpha == 0)
+                {
+                    fadeOut = false;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            Show();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            Hide();
+        }
+    }
+}
