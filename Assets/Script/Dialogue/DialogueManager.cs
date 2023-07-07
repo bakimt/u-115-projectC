f7c3e91a06812984b51b9e6b237a3a7533a74c49
Melisa Oktay <melisaoktay09@gmail.com>
2023-07-07 18:02:45 +0300

    Dialogue

--- Assets/Script/Dialogue/DialogueManager.cs
+++ Assets/Script/Dialogue/DialogueManager.cs
@@ -23,6 +23,26 @@ public class DialogueManager : MonoBehaviour
         activeMessage = 0;
 
         Debug.Log("Started conversations messages:" + messages.Length);
+        DisplayMessage();
+    }
+    
+    void DisplayMessage() {
+      Message messageToDisplay = currentMessages[activeMessage];
+      messageText.text = messageToDisplay.message;
+
+      Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+      actorName.text = actorToDisplay.name;
+
+    }
+
+    public void NextMessage() {
+        activeMessage++;
+        if (activeMessage < currentMessages.Length) {
+           DisplayMessage();
+          } else{
+            Debug.Log("Conversation ended");
+
+           }
     }
     void Start()
     {
@@ -34,6 +54,8 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      if (Input.GetKeyDown(KeyCode.Space)) {
+          NextMessage();
+      }   
     }
 }
