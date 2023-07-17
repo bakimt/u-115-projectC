252e5872b88671ab178f3df447de3e858416c178
Melisa Oktay <melisaoktay09@gmail.com>
2023-07-18 01:17:44 +0300

    dialoguescripts done

--- Assets/Script/dialogue 1/DialogueDeneme1.cs
+++ Assets/Script/dialogue 1/DialogueDeneme1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogueDede : MonoBehaviour
+public class DialogueDeneme : MonoBehaviour
 {
     public Canvas canvas;
     public GameObject textObject;
@@ -19,19 +19,23 @@ public class DialogueDede : MonoBehaviour
         dialogueArray = new string[]
         {
            
-            "Zephyra!",
-             "My brave girl! you found me! If you were a little more late I would've forgetten that you have even existed!",
-              "This place... it does strange things to me Zephyra I feel lost, I feel like I miss something but I don't even know what it is",
-               "We should head on our journey, to found ourselves, our purpose, so we don't feel lost anymore ",
-                "but before heading down our journey, I've lost my hat and my walking stick",
-                 "without my walking stick I can't walk to where we are headed next and without my hat...",
-                  "Well I suppose I can live without my hat but the hat has sentimental value... and of course it makes me look even more handsome than I already am",
-                   "You should head back to the labyrinth I've lost them there",
-                    "You got this my little girl we are so close to eternal happiness",
-                     "I love you",
-
-        
-           
+            "you must wonder where you are or how did you even get here",
+            "welcome to the realm of death and life",
+            "I'm Necrosia, a soul that passed away almost 100 years ago I've been here for a while now ",
+            "I looked like you before...",
+            "full of life and joy",
+            "getting stuck here didn't treat me very nicely",
+            "that's why I want to help you.. so you don't end up like me",
+            "This realm.. is not what every spirit goes through in their death journey",
+            "you're a special one my child, just like I was ",
+            "But I failed their tests.. I couldn't save my family from this realm and it's tests. and I'm in agony because of it everyday",
+            "I don't want you to end up like me I don't want you to go through what I went through",
+            "so I want to help you",
+            "your first mission, first one you should save is your cat, Luna",
+            "go up the path next to the church",
+            "there's a teleport point, it will teleport you to the necessary location",
+            "I believe in you, my child,",
+            
             
         };
     }
--- Assets/Script/dialogue 1/DialogueLabirent.cs
+++ Assets/Script/dialogue 1/DialogueLabirent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogueDeneme : MonoBehaviour
+public class DialogueLabirent : MonoBehaviour
 {
     public Canvas canvas;
     public GameObject textObject;
@@ -19,22 +19,18 @@ public class DialogueDeneme : MonoBehaviour
         dialogueArray = new string[]
         {
            
-            "you must wonder where you are or how did you even get here",
-            "welcome to the realm of death and life",
-            "I'm Necrosia, a soul that passed away almost 100 years ago I've been here for a while now ",
-            "I looked like you before...",
-            "full of life and joy",
-            "getting stuck here didn't treat me very nicely",
-            "that's why I want to help you.. so you don't end up like me",
-            "This realm.. is not what every spirit goes through in their death journey",
-            "you're a special one my child, just like I was ",
-            "But I failed their tests.. I couldn't save my family from this realm and it's tests. and I'm in agony because of it everyday",
-            "I don't want you to end up like me I don't want you to go through what I went through",
-            "so I want to help you",
-            "your first mission, first one you should save is your cat, Luna",
-            "go up the path next to the church",
-            "there's a teleport point, it will teleport you to the necessary location",
-            "I believe in you, my child,",
+            "wow you made it this far, I'm proud of you my child",
+            "this test the realm set up for you is kind of tricky, I'd have given more information if I could ",
+            "but I'm afraid I must let you be on your own on this one my child",
+            "but I may give you a little hint, a riddle so you can figure it out yourself",
+            "In shadows they soar, black as night, Their cawing echoes, taking flight. Intelligent and cunning, with feathers aglow,A harbinger of omens, what am I? Do you know?",
+            "they hold all the secrets you have to know for this puzzle my dear",
+            "good luck",
+            
+
+
+            
+           
             
             
         };
